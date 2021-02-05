c4897971c643d8732aa6ab33e5d117a146a18100
Nina Zurbulaeva <naina148@mail.ru>
2021-02-05 18:30:33 +0300

    feat: change condition's order, add ramdom function for 'n' |

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 'use strict'
 
-let n = 17;
+let n = Math.floor(Math.random() * 100 + 1); // загадывается случайное число
+//console.log("n = ", n);
+
+let isNumber = function(x) {
+    return !isNaN(parseFloat(x)) && isFinite(x);
+};
 
 let guessANum = function(){ 
     let num = prompt("Угадай число от 1 до 100");
-    //console.log("num: ",num,  "тип: ", typeof num);
-    if (+num === n) {
-        alert("Поздравляю, Вы угадали!!!")
-    } else if (num === null) {
+    //console.log("num =", num);
+    if (num === null) {
         alert("Игра окончена.");
-    } else if (num === "" || isNaN(num)){
+    } else if (!isNumber(num)) {       // если введенное значение не является числом: 
         alert("Введите число!")
         guessANum();
-    } else if (+num < n) {
-        alert("Загаданное число больше.");
-        guessANum();
-    } else {
-        alert("Загаданное число меньше.");
-        guessANum();
-    }
+    } else { 
+        if (+num === n) {
+            alert("Поздравляю, Вы угадали!!!");
+        } else if (+num < n) {
+            alert("Загаданное число больше.");
+            guessANum();
+        } else                                                                                                                                                                                                                                                                       {
+            alert("Загаданное число меньше.");
+            guessANum();
+        } 
+        // если нажата кнопка "Отмена"
+    } 
 }
 
 guessANum();
