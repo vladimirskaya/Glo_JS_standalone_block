eb8edf15f33bcd7a0678d6e4ca210d4418a50942
Nina Zurbulaeva <naina148@mail.ru>
2021-02-06 11:30:30 +0300

    feat: create closure  |

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 'use strict'
 
-let n = Math.floor(Math.random() * 100 + 1); // загадывается случайное число
-//console.log("n = ", n);
+let letsPlay = function(){
+    let n = Math.floor(Math.random() * 100 + 1); // загадывается случайное число
+    console.log("n = ", n);
 
-let guessANum = function(){ 
-    let num = prompt("Угадай число от 1 до 100");
-    //console.log("num =", num);
+    let guessANum = function(){ 
+       let num = prompt("Угадай число от 1 до 100");
+       //console.log("num =", num);
+    
+        // Создаем замыкание
+        let isNumber = function(x) {
+            return !isNaN(parseFloat(x)) && isFinite(x);
+        };
 
-    // Создаем замыкание
-    let isNumber = function(x) {
-        return !isNaN(parseFloat(x)) && isFinite(x);
-    };
-
-    if (num === null) {                 //нажата кнопка "Отмена" ил клавиша "Esc"
-        alert("Игра окончена.");
-    } else if (!isNumber(num)) {       // если введенное значение не является числом
-        alert("Введите число!");
-        guessANum();
-    } else { 
-        if (+num === n) {
-            alert("Поздравляю, Вы угадали!!!");
-        } else if (+num < n) {
-            alert("Загаданное число больше.");
+        if (num === null) {                 //нажата кнопка "Отмена" ил клавиша "Esc"
+            alert("Игра окончена.");
+        } else if (!isNumber(num)) {       // если введенное значение не является числом
+            alert("Введите число!");
             guessANum();
-        } else                                                                                                                                                                                                                                                                       {
-            alert("Загаданное число меньше.");
-            guessANum();
+        } else { 
+            if (+num === n) {
+                alert("Поздравляю, Вы угадали!!!");
+            } else if (+num < n) {
+                alert("Загаданное число больше.");
+                guessANum();
+            } else                                                                                                                                                                                                                                                                       {
+                alert("Загаданное число меньше.");
+                guessANum();
+            } 
         } 
-    } 
+    }
+guessANum();
 }
 
-guessANum();
+letsPlay();
